2d4881c01cdda800ef58a1cfc11836e2d7431f8e
AdhithyaGN <adhithyagnair97@gmail.com>
2024-06-13 00:37:57 +0530

    sentiments fetched on app run succesfully

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -1,23 +1,64 @@
 import streamlit as st
 from src.youtube.Sentiment_Analyser_app import sentiment_analysis
+from PIL import Image
 
 
 
 
-st.title('Youtube Sentiment Analysis App')
+
+
+
+
+st.title('Youtube CommentSentiment Analysis App')
+
 
 video_id=st.text_input('Enter the Video Id Here')
 
-submit=st.button('Click to see the results')
+submit=st.button('Click to see results')
+
+
+
+
+
+
+
 
 if submit :
+
+
+    st.header('Sentiment_Counts')
+
+    
 
     sentiment_obj=sentiment_analysis()
 
     video_response=sentiment_obj.video_response(video_id=video_id)
-    comments=sentiment_obj.comments(response=video_response)
+    #comments=sentiment_obj.comments(response=video_response)
+    data=sentiment_obj.sentiments_dataframe(data=video_response)
 
-    st.write(comments)
+    pos_count=data[data['sentiments']==2].shape[0]
+    neg_count=data[data['sentiments']==1].shape[0]
+    neu_count=data[data['sentiments']==0].shape[0]
+
+
+    pos_image=Image.open("C:/Users/S K E F/Desktop/GenAI/projects/Youtube_Comments_Analyser/icons/pos.png")
+    neg_image=Image.open("C:/Users/S K E F/Desktop/GenAI/projects/Youtube_Comments_Analyser/icons/neg.png")
+    neu_image=Image.open("C:/Users/S K E F/Desktop/GenAI/projects/Youtube_Comments_Analyser/icons/neu.png")
+
+    col1,col2,col3=st.columns(3)
+
+    with col1:
+        st.image(pos_image,width=100)
+        st.metric(label='Positive',value=pos_count)
+
+    with col2:
+        st.image(neu_image,width=100)
+        st.metric(label='Neutral',value=neu_count)
+    
+    with col3:
+        st.image(neg_image,width=100)
+        st.metric(label='Negative',value=neg_count)
+
 
 
 
--- src/youtube/Sentiment_Analyser_app.py
+++ src/youtube/Sentiment_Analyser_app.py
@@ -3,6 +3,9 @@ import googleapiclient.errors
 from dotenv import load_dotenv
 import os
 import pandas as pd
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+nltk.download('vader_lexicon')
 load_dotenv()
 
 GOOGLE_YOUTUBE_API=os.environ['GOOGLE_YOUTUBE_API']
@@ -17,31 +20,56 @@ class sentiment_analysis:
         self.api_service_name='youtube'
         self.api_version='v3'
         self.api=GOOGLE_YOUTUBE_API
+        self.comments=[]
 
         youtube=googleapiclient.discovery.build(serviceName=self.api_service_name,version=self.api_version,developerKey=self.api)
 
         request=youtube.commentThreads().list(part='snippet',
-                                              videoId=self.video_id,maxResults=100)
+                                              videoId=self.video_id,maxResults=1000)
         
-        response=request.execute()
+        while request:
+            response=request.execute()
+            for item in response['items']:
+                comment=item['snippet']['topLevelComment']['snippet']
+                self.comments.append({
+                    'author':comment['authorDisplayName'],
+                    'text':comment['textOriginal']
+                    
+                })
+            request=youtube.commentThreads().list_next(request,response)
 
-        return response
+        return pd.DataFrame(self.comments)
+        
+        
+
     
-    def comments(self,response):
-        self.response=response
+    
+    
+    def sentiments_dataframe(self,data):
+        self.df=data
 
-        comments=[]
-        for item in self.response['items']:
-            comment=item['snippet']['topLevelComment']['snippet']
-            comments.append([comment['authorDisplayName'],
-                             comment['publishedAt'],
-                             comment['updatedAt'],
-                             comment['likeCount'],
-                             comment['textDisplay']])
-            
-        df=pd.DataFrame(comments,columns=['author','published_at','updated_at','like_count','text'])
+        self.senti_analyser_obj=SentimentIntensityAnalyzer()
+        self.df['positive']=[self.senti_analyser_obj.polarity_scores(i)['pos'] for i in self.df['text']]
+        self.df['negative']=[self.senti_analyser_obj.polarity_scores(i)['neg'] for i in self.df['text']]
+        self.df['neutral']=[self.senti_analyser_obj.polarity_scores(i)['neu'] for i in self.df['text']]
+        self.df['compound']=[self.senti_analyser_obj.polarity_scores(i)['compound'] for i in self.df['text']]
 
-        return df
+        self.pol_values=list(self.df['compound'])
+
+        self.sentiment=[]
+
+        for i in self.pol_values:
+            if i>0.05:
+                self.sentiment.append(2)
+            elif i<-0.05:
+                self.sentiment.append(1)
+            else:
+                self.sentiment.append(0)
+
+        self.df['sentiments']=self.sentiment
+
+        return self.df
+    
     
 
 
